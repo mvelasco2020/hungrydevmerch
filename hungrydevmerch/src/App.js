0f569a4a75f765e91c11b06d6100fa718721e581
Mike Velasco <mvelasco2020@outlook.com>
2020-12-23 17:52:52 -0600

    initial commit - layout

---
+++ hungrydevmerch/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.jsx";
+
+function App() {
+    return (
+        <div>
+            <Homepage />
+        </div>
+    );
+}
+
+export default App;
