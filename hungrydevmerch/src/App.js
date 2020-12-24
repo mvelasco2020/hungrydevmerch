fe2879e72013a0c82cc3df79ca1209a397443c5a
Mike Velasco <mvelasco2020@outlook.com>
2020-12-24 15:54:31 -0600

    set up initial routing

--- hungrydevmerch/src/App.js
+++ hungrydevmerch/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import {Route, Switch } from 'react-router-dom'
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.jsx";
 
+const HatsPage = () => (
+    <div>
+        <h1>HatsPage</h1>
+    </div>
+)
+
 function App() {
     return (
         <div>
-            <Homepage />
+            <Switch>
+                <Route exact path='/' component={Homepage}/>
+                <Route  path='/hats' component={HatsPage}/>
+            </Switch>
         </div>
     );
 }
--- hungrydevmerch/src/components/menu-item/menu-item.jsx
+++ hungrydevmerch/src/components/menu-item/menu-item.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import './menu-item.scss'
+import React from "react";
+import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, id, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}></div>
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">Shop Now</span>
-        </div>
-    </div >
+const MenuItem = ({ title, imageUrl, id, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => {
+      history.push(`${match.url}${linkUrl}`);
+    }}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
