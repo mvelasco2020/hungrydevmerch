2792c9c914fc3627eed86abf770bf2493ebdb85d
Mike Velasco <mvelasco2020@outlook.com>
2020-12-27 17:37:24 -0600

    more authentication code in app.js

--- hungrydevmerch/src/App.js
+++ hungrydevmerch/src/App.js
@@ -1,22 +1,50 @@
 import React from "react";
-import {Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.jsx";
 import ShopPage from './pages/shop/shoppage.jsx'
 import Header from './components/header/header.jsx'
 import SignInSignUp from './pages/signin-signup/signin-signup.jsx'
-function App() {
-    return (
-        <div>
-            <Header/>
-            <Switch>
-                <Route exact path='/' component={Homepage}/>
-                <Route  path='/shop' component={ShopPage}/>
-                <Route  path='/signin' component={SignInSignUp}/>
-            </Switch>
-        </div>
-    );
-}
 
+import { auth } from '../src/firebase/firebase.utils'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({
+                currentUser: user
+            })
+            console.log(this.state.currentUser)
+        })
+    }
+
+    //close subscription to avoid memory leak
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={Homepage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/signin' component={SignInSignUp} />
+                </Switch>
+            </div>
+        );
+    }
+}
 export default App;
