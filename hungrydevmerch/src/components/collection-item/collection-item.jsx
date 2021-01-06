a83bf771fc4b91fd5499247ed3a44f6e9dd06a21
Mike Velasco <mvelasco2020@outlook.com>
2021-01-05 22:54:08 -0600

    add custom button and style to collection-item.jsx

--- hungrydevmerch/src/components/collection-item/collection-item.jsx
+++ hungrydevmerch/src/components/collection-item/collection-item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './collection-item.scss'
+import CustomButton from '../custom-button/custom-button'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
     return (
@@ -13,7 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-
+<CustomButton inverted>Add to cart</CustomButton>
 
         </div>
     )
