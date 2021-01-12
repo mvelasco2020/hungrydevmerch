5a67f3d2c5afe7356aa1b883415226b6c9ebf80c
Mike Velasco <mvelasco2020@outlook.com>
2021-01-11 23:06:18 -0600

    started working on checkoutpage component

--- hungrydevmerch/src/pages/checkout/checkout.jsx
+++ hungrydevmerch/src/pages/checkout/checkout.jsx
@@ -1,13 +1,45 @@
 import './checkout.scss'
 
+
 import React from 'react'
 
-const CheckoutPage = () => {
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+
+const CheckoutPage = ({ cartItems, cartTotal }) => {
     return (
-        <div>
-            checkoutpae
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+            {
+
+            }
+            <div className='total'>
+                <span>TOTAL: ${ cartTotal }</span>
+            </div>
         </div>
     )
 }
 
-export default CheckoutPage
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    cartTotal: selectCartTotal,
+})
+
+export default connect(mapStateToProps)(CheckoutPage)
--- hungrydevmerch/src/redux/cart/cart.selectors.js
+++ hungrydevmerch/src/redux/cart/cart.selectors.js
@@ -1,20 +1,24 @@
-import {createSelector} from 'reselect'
+import { createSelector } from 'reselect'
 
 //state) => state.reducerProp
 const selectCart = (state) => state.cart;
 
-export const selectCartItems 
-            = createSelector([selectCart],
-            cart => cart.cartItems);
+export const selectCartItems
+    = createSelector([selectCart],
+        cart => cart.cartItems);
 
-export const selectCartHidden 
-                = createSelector([selectCart],
-                    cart => cart.hidden);
+export const selectCartHidden
+    = createSelector([selectCart],
+        cart => cart.hidden);
 
 export const selectCartItemsCount
-         = createSelector([selectCartItems],
-            cartItems => cartItems.reduce((total, current)=> {
-                return total + current.quantity
-              },0))
+    = createSelector([selectCartItems],
+        cartItems => cartItems.reduce((total, current) => {
+            return total + current.quantity
+        }, 0))
 
-                                            
+export const selectCartTotal
+    = createSelector([selectCartItems],
+        cartTotal => cartTotal.reduce((total, current) => {
+            return total + current.quantity * current.price
+        }, 0))                                      
