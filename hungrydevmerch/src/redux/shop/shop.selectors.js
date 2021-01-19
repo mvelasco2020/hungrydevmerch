1fb37d55f8e7fbbd7ea881d70c6d663371d72b83
Mike Velasco <mvelasco2020@outlook.com>
2021-01-18 20:42:46 -0600

    changed shopdata to be object instead of array and modify shop selectors

--- hungrydevmerch/src/redux/shop/shop.selectors.js
+++ hungrydevmerch/src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5,
-}
-
 const selectCollections = state => state.shop;
 
 export const selectCollectionItems =
@@ -16,8 +8,7 @@ export const selectCollectionItems =
 
 export const selectCollectionCategory = collectionUrlParam => (
     createSelector([selectCollectionItems],
-        collections =>
-            collections.find(collection =>
-                collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+        collections => collections[collectionUrlParam]
+
     )
 )
